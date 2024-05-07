71a82799b44258dab64e1c52cf9abc104b89f3a5
PrincyChauhan <chauhanprincee7@gmail.com>
2024-05-07 18:22:00 +0530

    Change the version of flowbite-react to fix search in header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,12 @@ import About from "./pages/About";
 import Signin from "./pages/Signin";
 import Signup from "./pages/signup";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
