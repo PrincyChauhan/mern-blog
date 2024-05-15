c1fc30fba41d366d9c3c826bab3a53ca21a24b25
PrincyChauhan <chauhanprincee7@gmail.com>
2024-05-15 17:38:45 +0530

    Signin functionality

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -2,7 +2,7 @@ import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-export default function signup() {
+export default function Signup () {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -43,7 +43,7 @@ export default function signup() {
         <div className="flex-1">
         <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              PC's
+              PC&apos;s
             </span>
             Blog
         </Link>
